13a18a8c36b7d08b3623a370c69ef0d647cc9adf
show <show@outllook.com>
2019-07-07 22:32:14 +0800

    add

--- install_node8.11.4.sh
+++ install_node8.11.4.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
+mkdir /software/
+
+cd software
 # 下载安装包
 wget https://nodejs.org/dist/v8.11.4/node-v8.11.4-linux-x64.tar.xz
 
 #解压
 tar xvf node-v8.11.4-linux-x64.tar.xz 
 
-cd  node-v8.11.4-linux-x64
+move  node-v8.11.4-linux-x64    nodejs
 
-ln -s bin/node /usr/local/bin/node
-ln -s bin/npm /usr/local/bin/npm
+ln -s /software/nodejs/bin/node     /usr/local/bin
+ln -s /software/nodejs/bin/npm     /usr/local/bin
